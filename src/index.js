667c63df81a01788a74bb2e8fc0faf57e636fae9
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-03-23 10:12:52 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const formito=document.querySelector("form");
+formito.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  buildMyTaskito(e.target.task.value);
+  formito.reset();
+})
+})
+function buildMyTaskito(task){
+  const btn=document.createElement('button');
+  btn.textContent=' X'
+  btn.addEventListener('click',deleting)
+  const p=document.createElement("p");
+  p.textContent= `${task} `;
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#list").appendChild(p);
+}
+function deleting(e){
+  e.target.parentNode.remove();
+};
+document.addEventListener('DOMContentLoaded',()=>{
+  const formato=document.querySelector("#dates");
+  formato.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    buildDate(e.target.dating.value);
+    formato.reset();
+ })
+})
+function buildDate(dateInfo){
+  const btn=document.createElement('button');
+  btn.textContent="done";
+  btn.addEventListener('click',deleto)
+  const p=document.createElement('p');
+  p.textContent=dateInfo
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#listing").appendChild(p);
+}
+function deleto(e){
+  e.target.parentNode.remove();
+};
+
+
+
